e3471f4721b54153426af2204a782e7f820de000
Jacob Schwab <jacobschwab@tx.rr.com>
2019-05-16 12:57:16 -0500

    Update RGBLedTest.py

--- RGBLedTest.py
+++ RGBLedTest.py
@@ -14,43 +14,43 @@ GPIO.setup(B, GPIO.OUT)
 
 for n in range(25):
     i = randint(0,6)
-    if(i == 0):
+    if(i == 0): # Red
         GPIO.output(R, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(R, GPIO.LOW)
         sleep(0.1)
-    if(i == 1):
+    if(i == 1): # Green
         GPIO.output(G, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(G, GPIO.LOW)
         sleep(0.1)
-    if(i == 2):
+    if(i == 2): # Blue
         GPIO.output(B, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(B, GPIO.LOW)
         sleep(0.1)
-    if(i == 3):
+    if(i == 3): # Yellow
         GPIO.output(R, GPIO.HIGH)
         GPIO.output(G, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(R, GPIO.LOW)
         GPIO.output(G, GPIO.LOW)
         sleep(0.1)
-    if(i == 4):
+    if(i == 4): # Purple
         GPIO.output(R, GPIO.HIGH)
         GPIO.output(B, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(R, GPIO.LOW)
         GPIO.output(B, GPIO.LOW)
         sleep(0.1)
-    if(i == 5):
+    if(i == 5): # Cyan
         GPIO.output(G, GPIO.HIGH)
         GPIO.output(B, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(G, GPIO.LOW)
         GPIO.output(B, GPIO.LOW)
         sleep(0.1)
-    if(i == 6):
+    if(i == 6): # White
         GPIO.output(R, GPIO.HIGH)
         GPIO.output(G, GPIO.HIGH)
         GPIO.output(B, GPIO.HIGH)
